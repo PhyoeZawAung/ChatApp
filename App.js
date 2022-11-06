d773c349e84832c2e9fee3f43232e8d1c38f0688
PhyoeZawAung <phyoezawaung9696@gmail.com>
2022-11-06 09:37:35 +0630

    adding Profile Uploader

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import StackScreen from "./Screens/Navigator/Stack";
 import ImageUploader from "./Screens/ImageUploader";
 import auth from "@react-native-firebase/auth";
 import { useEffect, useState } from "react";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 const App = () => {
 
   return (
-    <StackScreen/>
-   //<ImageUploader/>
+    
+      <StackScreen/>
+
+    
+ 
   )
 }
 
--- Screens/Detail.js
+++ Screens/Detail.js
@@ -3,7 +3,8 @@ import React,{useState,useEffect} from "react";
 import { View, Text, Button,Image } from "react-native";
 import auth from "@react-native-firebase/auth";
 import store from "../Redux/stroe";
-import { Provider,useSelector } from "react-redux";
+import { Provider, useSelector } from "react-redux";
+import { Avatar } from "@rneui/base";
 const DetailScreen = ({ navigation }) => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
@@ -35,7 +36,17 @@ const DetailScreen = ({ navigation }) => {
             <Image source={{uri:user.photoURL}} style={{width:100,height:100}}/>
           <Text>{JSON.stringify(user.emailVerify)}</Text>
           <Text>{JSON.stringify(user.metadata)}</Text>
-          <Button title="signOut" onPress={() => SignOut()} />
+            <Button title="signOut" onPress={() => SignOut()} />
+            <Avatar
+          size={100}
+          rounded
+          source={{ uri: 'https://randomuser.me/api/portraits/women/57.jpg' }}
+          title="Bj"
+          containerStyle={{ backgroundColor: 'grey' }}
+        >
+              <Avatar.Accessory size={35}  
+                source={{ uri: 'https://randomuser.me/api/portraits/women/57.jpg' }}/>
+              </Avatar>
           </View>
       ) : (
           <View>
