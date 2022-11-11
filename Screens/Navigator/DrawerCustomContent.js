39aae4737d204186b38130882f60313348badec0
PhyoeZawAung <phyoezawaung9696@gmail.com>
2022-11-11 16:26:57 +0630

    modyfying the drawer

--- Screens/Navigator/DrawerCustomContent.js
+++ Screens/Navigator/DrawerCustomContent.js
@@ -3,9 +3,9 @@ import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
 import {View, Text, Image} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {Provider, useSelector} from 'react-redux';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {Icon} from '@rneui/base';
 import store from '../../Redux/stroe';
-import { TEST_ID } from 'react-native-gifted-chat';
+import {TEST_ID} from 'react-native-gifted-chat';
 
 const CustomContent = props => {
   const [initializing, setInitializing] = useState(true);
@@ -16,7 +16,7 @@ const CustomContent = props => {
   useEffect(() => {
     const user = auth().currentUser;
     if (user != null) {
-      setUser(user)
+      setUser(user);
       console.log(JSON.stringify(user));
     }
   }, []);
@@ -31,54 +31,71 @@ const CustomContent = props => {
   return (
     <Provider store={store}>
       {user ? (
-       <View style={{flex: 1}}>
-       <DrawerContentScrollView style={{alignSelf: 'center'}}>
-         <View>
-           <Image
-             source={{uri: user.photoURL}}
-             style={{width: 100, height: 100, borderRadius: 100}}
-           />
-           <Text>{user.email}</Text>
-           <View style={{flexDirection: 'row'}}>
-             <Text>{firstname}</Text>
-             <Text>{lastName}</Text>
-           </View>
-         </View>
-       </DrawerContentScrollView>
-       <DrawerContentScrollView {...props}>
-         <View>
-           <DrawerItem
-             icon={({color, size}) => (
-               <Icon name="users" color={color} size={size} />
-             )}
-             label="Home"
-             onPress={() => props.navigation.navigate('Index')}></DrawerItem>
-           <DrawerItem
-             icon={({color, size}) => (
-               <Icon name="users" color={color} size={size} />
-             )}
-             label="Profile"
-             onPress={() => props.navigation.navigate('Profile')}></DrawerItem>
-           <DrawerItem
-             icon={({color, size}) => (
-               <Icon name="users" color={color} size={size} />
-             )}
-             label="Contacts"
-             onPress={() => props.navigation.navigate('Contact')}></DrawerItem>
-           <DrawerItem
-             icon={({color, size}) => (
-               <Icon name="users" color={color} size={size} />
-             )}
-             label="Log Out"
-             onPress={() => SignOut()}></DrawerItem>
-         </View>
-       </DrawerContentScrollView>
-     </View>
-      ) : (
-        null
-      )
-      }
-     
+        <View style={{flex: 1, backgroundColor: '#4F3B70'}}>
+          <View style={{alignItems: 'center', paddingTop: 20}}>
+            <Image
+              source={{uri: user.photoURL}}
+              style={{width: 100, height: 100, borderRadius: 100}}
+            />
+            <Text
+              style={{
+                color: '#fff',
+                paddingTop: 20,
+                fontWeight: 'bold',
+                fontSize: 23,
+              }}>
+              {user.displayName}
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text>{firstname}</Text>
+              <Text>{lastName}</Text>
+            </View>
+          </View>
+
+          <DrawerContentScrollView {...props}>
+            <View style={{justifyContent: 'space-between', height: 450}}>
+              <View style={{paddingLeft: 30}}>
+                <DrawerItem
+                  icon={({color, size}) => (
+                    <Icon name="home" color={color} size={size} />
+                  )}
+                  label="Home"
+                  inactiveTintColor="#fff"
+                  onPress={() =>
+                    props.navigation.navigate('Index')
+                  }></DrawerItem>
+                <DrawerItem
+                  icon={({color, size}) => (
+                    <Icon name="person" color={color} size={size} />
+                  )}
+                  label="Profile"
+                  inactiveTintColor="#fff"
+                  onPress={() =>
+                    props.navigation.navigate('Profile')
+                  }></DrawerItem>
+                <DrawerItem
+                  icon={({color, size}) => (
+                    <Icon name="people" color={color} size={size} />
+                  )}
+                  label="Contacts"
+                  inactiveTintColor="#fff"
+                  onPress={() =>
+                    props.navigation.navigate('Contact')
+                  }></DrawerItem>
+              </View>
+
+              <DrawerItem
+                style={{paddingLeft: 30}}
+                icon={({color, size}) => (
+                  <Icon name="logout" color={color} size={size} />
+                )}
+                label="Log Out"
+                inactiveTintColor="#fff"
+                onPress={() => SignOut()}></DrawerItem>
+            </View>
+          </DrawerContentScrollView>
+        </View>
+      ) : null}
     </Provider>
   );
 };
