2e89a02b1c47c2f564870f66e0fd07cbc4cf3265
PhyoeZawAung <phyoezawaung9696@gmail.com>
2022-11-11 15:33:58 +0630

    Removing The Start Screen and Change to Login Screen

--- Screens/StartScreen.js
+++ Screens/StartScreen.js
@@ -1,10 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
-
-
-const StartScreen = ({ navigation }) => {
+const StartScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View>
@@ -14,36 +12,40 @@ const StartScreen = ({ navigation }) => {
         <Pressable
           style={styles.button}
           onPress={() => {
-            navigation.navigate("Login")
+            navigation.navigate('Login');
           }}>
-          <Text style={{ color: '#fff', fontWeight: 'bold' }}>Login with Email</Text>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>
+            Login with Email
+          </Text>
         </Pressable>
-        <Pressable onPress={() => { navigation.navigate("Forgot") }}>
+        <Pressable
+          onPress={() => {
+            navigation.navigate('Forgot');
+          }}>
           <Text style={styles.text}>Forgotten Password?</Text>
         </Pressable>
-
       </View>
       <View>
         <View style={styles.signup}>
-          <Text style={styles.text}>New User?  </Text>
-          <Pressable onPress={() => { navigation.navigate("SignUp") }}>
+          <Text style={styles.text}>New User? </Text>
+          <Pressable
+            onPress={() => {
+              navigation.navigate('SignUp');
+            }}>
             <Text style={styles.signUpText}>Sign Up</Text>
           </Pressable>
         </View>
-
-
       </View>
-
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#9b5de0',
     flex: 1,
     padding: 30,
-    justifyContent: "space-between",
-    alignItems: "center",
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   button: {
     width: 350,
@@ -54,17 +56,17 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   signup: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   text: {
-    color: "#fff",
-    fontWeight: "bold",
+    color: '#fff',
+    fontWeight: 'bold',
   },
   signUpText: {
-    color: "#46eeaa",
-    fontWeight: "bold",
-    fontSize: 18
-  }
-})
+    color: '#46eeaa',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
 export default StartScreen;
